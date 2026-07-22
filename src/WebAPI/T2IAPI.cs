28b8c444f94af0ed6ab077bd24fb2e7a9ca187e3
Reclusiarch-Grimaldus <91054049+Reaper176@users.noreply.github.com>
2026-07-22 04:09:30 -0500

    fix: use current T2I parameter count owner

--- src/WebAPI/T2IAPI.cs
+++ src/WebAPI/T2IAPI.cs
@@ -313,7 +313,7 @@ public static class T2IAPI
         Logs.Info($"User {session.User.UserID} requested {images} image{(images == 1 ? "" : "s")} with model '{user_input.Get(T2IParamTypes.Model)?.Name}'...");
         if (Logs.MinimumLevel <= Logs.LogLevel.Verbose)
         {
-            Logs.Verbose($"User {session.User.UserID} above image request had parameter count: {user_input.ValuesInput.Count}");
+            Logs.Verbose($"User {session.User.UserID} above image request had parameter count: {user_input.InternalSet.ValuesInput.Count}");
         }
         List<T2IEngine.ImageOutput> imageSet = [];
         List<Task> tasks = [];
